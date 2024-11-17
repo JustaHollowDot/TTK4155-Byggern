e77dd6ff5e5f494b2a42931ac01b540809609086
JustaHollowDot <emil@hallingstad.me>
2024-11-17 02:39:12 +0100

    changes for handin

--- node_2/src/pwm.c
+++ node_2/src/pwm.c
@@ -10,16 +10,16 @@
 uint32_t last_dc;
 
 void pwm_test_2() {
-    PMC->PMC_PCER1 |= PMC_PCER1_PID36;                    // PWM power on , see datasheet page 38
+    PMC->PMC_PCER1 |= PMC_PCER1_PID36;                    // PWM power on
     PWM->PWM_DIS = PWM_DIS_CHID0;                         // Disable PWM channel 0
     PMC->PMC_PCER0 |= PMC_PCER0_PID12;                    // PIOA power on
 
     PIOB->PIO_PDR |= PIO_PDR_P12;                         // The GPIO don't drive the pin, this is the peripheral
-    PIOB->PIO_ABSR |= PIO_ABSR_P12;                       // Set PWM pin perhipheral type B , datasheet page 974
+    PIOB->PIO_ABSR |= PIO_ABSR_P12;                       // Set PWM pin perhipheral type B
 
-    PWM->PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);    // Set the PWM clock rate to 2MHz (84MHz/42). Adjust DIVA for the resolution you are looking for
+    PWM->PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);    // Set the PWM clock rate to 2MHz (84MHz/42)
     PWM->PWM_CH_NUM[0].PWM_CMR = PWM_CMR_CPRE_CLKA;      // The period is left aligned, clock source as CLKA on channel 0
-    PWM->PWM_CH_NUM[0].PWM_CPRD = 4000;                  // Channel 0 : Set the PWM frequency (84MHz/42)/PWM_CPRD = 100KHz ;
+    PWM->PWM_CH_NUM[0].PWM_CPRD = 4000;                  // Channel 0 : Set the PWM frequency (84MHz/42)/PWM_CPRD = 50 Hz
     PWM->PWM_CH_NUM[0].PWM_CDTY = 0;                     // Channel 0: Set the PWM duty cycle to x%= (CDTY/ CPRD)  * 100 % = 50%;
 
     PWM->PWM_ENA = PWM_ENA_CHID0;
